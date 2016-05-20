6caf950796a96538c33b467496b807cfed8ee421
Ben Johnson <ben@gophronesis.com>
2016-05-20 18:33:37 -0400

    cleaning code

--- features.py
+++ features.py
@@ -25,58 +25,47 @@ if params['gpu']:
 else:
     caffe.set_mode_cpu()
 
-
-def learn_transform(params, feats):
-    normalize(feats)
-    pca = PCA(params['dimension'], whiten=True)
-    pca.fit(feats)
-    pickle.dump(pca,open(params['pca_model'] + '_' + params['dataset'] + '.pkl','wb'))
-
-
 class Extractor():
-
-    def __init__(self,params):
-        
-        self.dimension = params['dimension']
-        self.dataset   = params['dataset']
-        self.pooling   = params['pooling']
+    
+    def __init__(self, params):    
+        self.dimension      = params['dimension']
+        self.dataset        = params['dataset']
+        self.pooling        = params['pooling']
+        self.layer          = params['layer']
         
         self.database_list = open(params['frame_list'],'r').read().splitlines()
         
-        self.layer         = params['layer']
-        self.save_db_feats = params['database_feats']
-        
-        print "Extracting from:", params['net_proto']
         self.net = caffe.Net(params['net_proto'], params['net'], caffe.TEST)
-
-    def extract_feat_image(self,image):
-        im = cv2.imread(image)
-        scores, boxes = test_ops.im_detect(self.net, im, boxes=None)
-        return self.net.blobs[self.layer].data.squeeze()
-
+    
     def pool_feats(self,feat):        
         f = np.max if self.pooling == 'max' else np.sum
         return f(f(feat, axis=2), axis=1)
-        
-    def save_feats_to_disk(self):
-        xfeats  = np.zeros((len(self.database_list), self.dimension))
+    
+    def image2features(self,im):
+        scores, _ = test_ops.im_detect(self.net, im, boxes=None)
+        feats = self.net.blobs[self.layer].data.squeeze()
+        return self.pool_feats(feats)
+    
+    def save_feats_to_disk(self, PRINT_INTERVAL=5):
+        pooled_feats = np.zeros((len(self.database_list), self.dimension))
         
         t0 = time.time()
         for i,frame in enumerate(self.database_list):
-            feat = self.extract_feat_image(frame)
-            feat = self.pool_feats(feat)
-            xfeats[i,:] = feat
+            pooled_feats[i,:] = self.image2features(cv2.imread(frame))
             
-            if not counter % 5:
+            if not counter % PRINT_INTERVAL:
                 print counter, '/', len(self.database_list), time.time() - t0
         
-        pickle.dump(xfeats, open(self.save_db_feats, 'wb'))
+        return pooled_feats
         
 
-if __name__ == "__main__":
-    params = get_params()
+if __name__ == "__main__":    
+    # Compute and save pooled_feats
+    pooled_feats = Extractor(params).save_feats_to_disk()
+    pickle.dump(pooled_feats, open(params['database_feats'], 'wb'))
     
-    Extractor(params).save_feats_to_disk()
-    
-    feats = pickle.load(open(params['database_feats'],'rb'))
-    learn_transform(params,feats)
+    # Learn whitening transformation from pooled_feats
+    normalize(pooled_feats)
+    pca = PCA(params['dimension'], whiten=True)
+    pca.fit(pooled_feats)
+    pickle.dump(pca, open('%s_%s.pkl' % (params['pca_model'], params['dataset']), 'wb'))
--- ranker.py
+++ ranker.py
@@ -2,7 +2,6 @@ import os
 import time
 import random
 import pickle
-
 import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.preprocessing import normalize
@@ -11,7 +10,7 @@ from params import get_params
 class Ranker():
     def __init__(self,params):
         
-        self.dataset = params['dataset']
+        self.dataset       = params['dataset']
         self.other_dataset = 'paris' if self.dataset == 'oxford' else 'oxford'
         
         self.database_images = params['database_images']
@@ -25,73 +24,53 @@ class Ranker():
         self.database_list = np.array(open(params['frame_list'],'r').read().splitlines())
         self.query_names   = open(params['query_list'],'r').read().splitlines()
         
-        self.pca      = pickle.load(open('%s_%s.pkl' % (params['pca_model'], self.other_dataset), 'rb'))
-        self.db_feats = pickle.load(open(params['database_feats'],'rb'))
-            
-        normalize(self.db_feats)
-        if self.pooling is 'sum':
-            self.db_feats = self.pca.transform(self.db_feats)
-            normalize(self.db_feats)
-    
-    def get_query_vectors(self):
-        self.query_feats = np.zeros((len(self.query_names),self.dimension))
-        
-        for i,query in enumerate(self.query_names):
-            
-            query_file, box       = self.query_info(query)
-            self.query_feats[i,:] = self.db_feats[np.where(self.database_list == query_file)]
-            
-            # add top elements of the ranking to the query
-            if self.stage is 'QE':
-                
-                with open(os.path.join(self.rankings_dir,os.path.basename(query.split('_query')[0]) +'.txt'),'r') as f:
-                    ranking = f.read().splitlines()
-                
-                for i_q in range(self.N_QE):
-                    
-                    imfile = ranking[i_q]
-                    
-                    if self.dataset is 'paris':
-                        imname = os.path.join(self.database_images, imfile.split('_')[1], imfile + '.jpg')
-                    elif self.dataset is 'oxford':
-                        imname = os.path.join(self.database_images, imfile + '.jpg')
-                    
-                    # find feature and add to query
-                    feat = self.db_feats[np.where(self.database_list == imname)].squeeze()
-                    self.query_feats[i,:] += feat
-                
-                # find feature and add to query
-        
-        normalize(self.query_feats)
-                
-    def query_info(self,filename):
+    def query_info(self, filename):
         data = np.loadtxt(filename, dtype="str")
         
-        bbox = data[1:].astype(float).astype(int)
-        
         if self.dataset == 'paris':
-            query = os.path.join(self.database_images,query.split('_')[1], data[0] + '.jpg')
+            query_file = os.path.join(self.database_images, data[0].split('_')[1], data[0] + '.jpg')
         elif self.dataset == 'oxford':
-            query = os.path.join(self.database_images, data[0].split('oxc1_')[1] + '.jpg')
+            query_file = os.path.join(self.database_images, data[0].split('oxc1_')[1] + '.jpg')
         
-        return query, bbox 
+        return query_file
     
-    def write_rankings(self,distances):
+    def get_query_vectors(self, db_feats):
+        query_feats = np.zeros((len(self.query_names),self.dimension))
+        
+        for i,filename in enumerate(self.query_names):
+            query_file       = self.query_info(filename)
+            query_feats[i,:] = db_feats[np.where(self.database_list == query_file)]
+        
+        normalize(query_feats)
+        return query_feats
+    
+    def write_rankings(self, distances):
         for i,query in enumerate(self.query_names):
             scores   = distances[i,:]
             rankings = self.database_list[np.argsort(scores)]
-            savefile = open(os.path.join(self.rankings_dir,os.path.basename(query.split('_query')[0]) +'.txt'),'w')    
+            savefile = open(os.path.join(self.rankings_dir, os.path.basename(query.split('_query')[0]) +'.txt'),'w')    
             for ranking in rankings:
                 savefile.write(os.path.basename(rankings).split('.jpg')[0] + '\n')
             
             savefile.close()
+          
+
+def load_database(params):
+    pca      = pickle.load(open('%s_%s.pkl' % (params['pca_model'], self.other_dataset), 'rb'))
+    db_feats = pickle.load(open(params['database_feats'], 'rb'))
+    normalize(db_feats)
+    if self.pooling is 'sum':
+        db_feats = pca.transform(db_feats)
+        normalize(db_feats)
     
-    def rank(self):
-        self.get_query_vectors()
-        distances = pairwise_distances(self.query_feats,self.db_feats,self.distance, n_jobs=-1)
-        self.write_rankings(distances)
-      
+    return db_feats
+
 
 if __name__ == "__main__":
     params = get_params()
-    Ranker(params).rank()
+
+    db_feats    = load_database(params)
+    query_feats = Ranker(params).get_query_vectors(db_feats)
+    
+    distances   = pairwise_distances(query_feats, db_feats, params['distance'], n_jobs=-1)
+    ranker.write_rankings(distances)
