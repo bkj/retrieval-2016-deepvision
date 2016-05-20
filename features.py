9fca3f5006e7d1898c44f146e226e1d2ed9f2be3
Ben Johnson <ben@gophronesis.com>
2016-05-20 17:55:36 -0400

    cleaning code

--- features.py
+++ features.py
@@ -1,9 +1,10 @@
-import sys, os
+import os
+import sys
 import cv2
 import time
+import pickle
 import numpy as np
 from params import get_params
-import pickle
 from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA
 
@@ -14,106 +15,73 @@ sys.path.insert(0, os.path.join(params['fast_rcnn_path'],'caffe-fast-rcnn', 'pyt
 sys.path.insert(0, os.path.join(params['fast_rcnn_path'],'lib'))
 
 import caffe
-from fast_rcnn.config import cfg
 import test as test_ops
+from fast_rcnn.config import cfg
+
+cfg.TEST.HAS_RPN = True
+if params['gpu']:
+    caffe.set_mode_gpu()
+    caffe.set_device(0)
+else:
+    caffe.set_mode_cpu()
 
 
-def learn_transform(params,feats):
-    
+def learn_transform(params, feats):
     normalize(feats)
-    pca = PCA(params['dimension'],whiten=True)
-    
+    pca = PCA(params['dimension'], whiten=True)
     pca.fit(feats)
-        
     pickle.dump(pca,open(params['pca_model'] + '_' + params['dataset'] + '.pkl','wb'))
-    
+
 
 class Extractor():
 
     def __init__(self,params):
 
         self.dimension = params['dimension']
-        self.dataset = params['dataset']
-        self.pooling = params['pooling']
-        # Read image lists
-        with open(params['query_list'],'r') as f:
-            self.query_names = f.read().splitlines()
+        self.dataset   = params['dataset']
+        self.pooling   = params['pooling']
 
-        with open(params['frame_list'],'r') as f:
-            self.database_list = f.read().splitlines()
+        self.database_list = open(params['frame_list'],'r').read().splitlines()
 
         # Parameters needed
-        self.layer = params['layer']
+        self.layer         = params['layer']
         self.save_db_feats = params['database_feats']
-
-        # Init network
-        if params['gpu']:
-            caffe.set_mode_gpu()
-            caffe.set_device(0)
-        else:
-            caffe.set_mode_cpu()
+        
         print "Extracting from:", params['net_proto']
-        cfg.TEST.HAS_RPN = True
         self.net = caffe.Net(params['net_proto'], params['net'], caffe.TEST)
 
     def extract_feat_image(self,image):
-
         im = cv2.imread(image)
-        
-        scores, boxes = test_ops.im_detect(self.net, im, boxes = None)
-        feat = self.net.blobs[self.layer].data
+        scores, boxes = test_ops.im_detect(self.net, im, boxes=None)
+        return self.net.blobs[self.layer].data
 
-        return feat
-        
-
-    def pool_feats(self,feat):
-        
-        if self.pooling is 'max':
-            
-            feat = np.max(np.max(feat,axis=2),axis=1)
-        else:
-            
-            feat = np.sum(np.sum(feat,axis=2),axis=1)
-            
-        return feat
+    def pool_feats(self,feat):        
+        f = np.max if self.pooling == 'max' else np.sum
+        return f(f(feat, axis=2), axis=1)
         
     def save_feats_to_disk(self):
-                
-        print "Extracting database features..."
-        t0 = time.time()
+        xfeats  = np.zeros((len(self.database_list),self.dimension))
         counter = 0
-
-        # Init empty np array to store all databsae features
-        xfeats = np.zeros((len(self.database_list),self.dimension))
-        
+        t0 = time.time()
         for frame in self.database_list:
             counter +=1
             
             # Extract raw feature from cnn
             feat = self.extract_feat_image(frame).squeeze()
+            print feat
+            xfeats[counter-1,:] = self.pool_feats(feat)
             
-            # Compose single feature vector
-            feat = self.pool_feats(feat)
-            
-            # Add to the array of features
-            xfeats[counter-1,:] = feat
-            
-            # Display every now and then
-            if counter%50 == 0:
+            if not counter % 5:
                 print counter, '/', len(self.database_list), time.time() - t0
         
         
-        # Dump to disk
-        pickle.dump(xfeats,open(self.save_db_feats,'wb'))
-        
-        print " ============================ "
+        pickle.dump(xfeats, open(self.save_db_feats,'wb'))
         
 
 if __name__ == "__main__":
     params = get_params()
     
-    E = Extractor(params)
-    E.save_feats_to_disk()
+    Extractor(params).save_feats_to_disk()
     
     feats = pickle.load(open(params['database_feats'],'rb'))
     learn_transform(params,feats)
--- params.py
+++ params.py
@@ -1,60 +1,49 @@
 import os
 
 def get_params():
-
+    
     params = {}
     
     # Parameters
     params['dataset'] = 'oxford'
     
-    params['stage']= 'rerank' # if 'rerank', display will include detections. if 'rerank2nd', reranking will be performed with QE top N locations
+    params['stage']               = 'rerank' # if 'rerank', display will include detections. if 'rerank2nd', reranking will be performed with QE top N locations
     params['use_regressed_boxes'] = False
-    params['use_class_scores'] = False
-    params['gpu'] = True # Applies to feature extraction and reranking
-    params['distance'] = 'cosine'
-    params['pooling'] = 'max'
+    params['use_class_scores']    = False
+    params['gpu']                 = True # Applies to feature extraction and reranking
+    params['distance']            = 'cosine'
+    params['pooling']             = 'max'
     
     params['fast_rcnn_path'] = '../faster-rcnn/'
-    params['net'] = 'data/models/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel'
-    params['net_proto'] ='data/models/test.prototxt'
+    params['net']            = 'data/models/faster_rcnn_models/VGG16_faster_rcnn_final.caffemodel'
+    params['net_proto']      = 'data/models/test.prototxt'
             
-    params['layer'] = 'conv5_3'
+    params['layer']     = 'conv5_3'
     params['dimension'] = 512 # number of filters in the used layer
       
-    params['K'] = 100000 # Number of elements to consider for mAP (high number means all elements are evaluated)
+    params['K']          = 100000 # Number of elements to consider for mAP (high number means all elements are evaluated)
     params['num_rerank'] = 100 # Top elements to consider for reranking
-    params['size_box'] = 15 # for display 
-    params['N_QE'] = 5 # number of elements for query expansion
-    params['N_display'] = 10 # top N ranking to display
-    params['figsize'] = (80,40)
-    
+    params['size_box']   = 15 # for display 
+    params['N_QE']       = 5 # number of elements for query expansion
+    params['N_display']  = 10 # top N ranking to display
+    params['figsize']    = (80,40)
     
     if params['dataset'] is 'oxford':
-        
-        params['query_names'] = ["all_souls", "ashmolean", "balliol","bodleian", "christ_church", "cornmarket","hertford","keble","magdalen","pitt_rivers","radcliffe_camera"]
-        
-        params['database_images'] = 'data/images/oxford/data/' # oxford
+        params['query_names']       = ["all_souls", "ashmolean", "balliol","bodleian", "christ_church", "cornmarket","hertford","keble","magdalen","pitt_rivers","radcliffe_camera"]
+        params['database_images']   = 'data/images/oxford/data/' # oxford
         params['ground_truth_file'] = 'data/images/oxford/groundtruth'
-        
     elif params['dataset'] is 'paris':
-        
-        params['query_names'] = ["defense", "eiffel","invalides", "louvre", "moulinrouge","museedorsay","notredame","pantheon","pompidou","sacrecoeur", "triomphe"]
-        
-        params['database_images'] = 'data/images/paris/data/paris' # paris
+        params['query_names']       = ["defense", "eiffel","invalides", "louvre", "moulinrouge","museedorsay","notredame","pantheon","pompidou","sacrecoeur", "triomphe"]
+        params['database_images']   = 'data/images/paris/data/paris' # paris
         params['ground_truth_file'] = 'data/images/paris/groundtruth'
         
     
-    params['query_list'] = 'data/imagelists/query' +'_' +params['dataset'] +  '.txt' # A txt file  
-    params['frame_list'] = 'data/imagelists/' + params['dataset'] + '.txt' # A txt file - trecvid
-
-    # Storage
-    params['rankings_dir'] = 'data/rankings/'+ params['dataset']
-    
-
+    params['query_list']     = 'data/imagelists/query' +'_' +params['dataset'] +  '.txt' # A txt file  
+    params['frame_list']     = 'data/imagelists/' + params['dataset'] + '.txt' # A txt file - trecvid
+    params['rankings_dir']   = 'data/rankings/'+ params['dataset']
     params['database_feats'] = 'data/features/' + params['dataset'] + '_' + params['layer'] + '_' + params['pooling']+ '.pkl' # This is a single pickle file
-    params['pca_model'] = 'data/pca/' + params['layer'] + '_' + params['pooling'] 
- 
-    params['figures_path'] = 'data/figures/'
+    params['pca_model']      = 'data/pca/' + params['layer'] + '_' + params['pooling'] 
+    params['figures_path']   = 'data/figures/'
     params['reranking_path'] = 'data/reranking/'
     
     params['paris_corrupt_list'] = ['paris_louvre_000136.jpg',
@@ -80,11 +69,7 @@ def get_params():
     
     
     list_of_dirs = ['data/imagelists','data/rankings/','data/features','data/rankings','data/rankings/oxford','data/rankings/paris','data/reranking','data/pca','data/figures']
-    
-    for _dir in list_of_dirs:
-        if not os.path.isdir(_dir):
-            os.makedirs(_dir)    
-            
+    _ = [os.makedirs(_dir) for _dir in list_of_dirs if not os.path.isdir(_dir)]            
     return params
 
 
